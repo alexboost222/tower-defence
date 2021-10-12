279527f4e4ca64060dbc5cbdfc766c299c23cb84
alexboost222 <alexboost222@gmail.com>
2021-10-13 01:49:58 +0300

    add: projectile destoroy after collision

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
 
 [DisallowMultipleComponent]
+[RequireComponent(typeof(Collider))]
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float speed;
+
+    private Collider _collider;
     
     public Vector3 Direction { get; set; }
 
-    private void Update() => transform.Translate(speed * Time.deltaTime * Direction);
+    private void Awake() => _collider = GetComponent<Collider>();
+
+    private void Update()
+    {
+        _collider.isTrigger = true;
+        transform.Translate(speed * Time.deltaTime * Direction);
+    }
+
+    private void OnTriggerEnter(Collider other) => Destroy(gameObject);
 }
